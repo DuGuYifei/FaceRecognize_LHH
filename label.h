cf3c70356120c4eef685b5ec023d2f939e49c994
DuGuYifei <liuyifei97@163.com>
2022-06-16 00:52:04 +0200

    1st submit

---
+++ label.h
@@ -0,0 +1,28 @@
+//#pragma once
+//
+////a1 - face containing flag : (1 - with face, 0 - without face),
+////a2 - image number in current class (person)beginning from 0,
+////a3 - class (person)number beginning from 0,
+////a4 - sex(0 - woman, 1 - man)
+////a5 - race(0 - white, 1 - negro, 2 - indian, ...)
+////a6 - age(0 - baby, 1 - young, 2 - middle - age, 3 - old)
+////a7 - binokulars(0 - without, 1 - transparent, 2 - dark)
+////a8 - emotional expression(not state!) (0 - sad, 1 - neutral, 2 - happy)
+//
+//class Label
+//{
+//public:
+//	int face;
+//	int img;
+//	int people;
+//	int sex;
+//	int race;
+//	int age;
+//	int binokulars;
+//	int emotion;
+//	Label(int label[8]);
+//
+//};
+//
+//Label::Label(int label[8]):face(label[0]),img(label[1]),people(label[2]),sex(label[3]),race(label[4]),age(label[5]),binokulars(label[6]),emotion(label[7])
+//{};
